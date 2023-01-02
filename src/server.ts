30e7b595e83b9fb8b93511df4753c5a52eaaf578
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-01-02 08:50:48 -0300

    initial config

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  return res.send("Hello World!");
+});
+
+app.listen(3333, () => {
+  console.log("Server is running. Open it at localhost:3333");
+});
