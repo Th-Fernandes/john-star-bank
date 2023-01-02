996b5a02313d95ecb948cb931aded836ec8fbff6
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-01-02 16:11:31 -0300

    routes: signIn user data validation

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,26 +1,49 @@
 import { prisma } from '../lib/prisma';
 import express, { Request, Response } from 'express';
 import { User } from '@prisma/client';
+import { z } from 'zod';
 
 const router = express.Router();
 
-router.get('/', async ( req:Request, res:Response ) => {
+router.get('/signIn', async ( req:Request, res:Response ) => {
 	const { username, password } = req.body;
-	let createUser: User;
 
-	try {
-		createUser = await prisma.user.create({
-			data: {
-				username,
-				password,
-				account: { create: {} }
-			}
+	function validateUserData() {
+		const user = z.object({
+			username: z.string()
+				.min(3, {message: 'nome do usuário deve possuir ao menos 3 caracteres'})
+				.max(30, {message: 'nome do usuário deve possuir no máximo 30 caracteres'}), 
+			password: z.string()
 		});
-	} catch (error) {
-		return res.status(400).json({message: 'não foi possível efeituar cadastro', error});
+  
+		try {
+			user.parse({username, password});
+		} catch (error) {
+			return res.status(400).json({error});
+		}
 	}
 
-	return res.json(createUser);
+	validateUserData();
+
+	let createdUser: User | false = false;
+
+	async function createUserOnDb() {
+		try {
+			createdUser = await prisma.user.create({
+				data: {
+					username,
+					password,
+					account: { create: {} }
+				}
+			});
+		} catch (error) {
+			return res.status(400).json({message: 'não foi possível efeituar cadastro', error});
+		}
+	}
+
+	await createUserOnDb();
+
+	return res.json(createdUser);
 });
 
 export default router;
